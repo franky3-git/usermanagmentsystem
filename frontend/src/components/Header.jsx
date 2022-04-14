7473deebcea86253a49caa220b880c69e06aa710
franky3-git <frankndounga@yahoo.fr>
2022-04-14 12:15:53 +0100

    fix the selected link background color

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link} from 'react-router-dom';
 
 import './css/header.css';
 
 const Header = () => {
+	const [selectedLink, setSelectedLink] = useState('home') 
+	
 	return (
 		<header>
 			<div className="title">user management system</div>
 			<nav>
-				<Link to="/" className="link">Home</Link>
-				<Link to="/adduser" className="link">add user</Link>
-				<Link to="/about" className="link">about</Link>
+				<Link to="/" className={`link ${selectedLink === 'home' ? 'active' : ''}`} onClick={() => setSelectedLink('home')}>Home</Link>
+				<Link to="/adduser" className={`link ${selectedLink === 'add' ? 'active' : ''}`} onClick={() => setSelectedLink('add')}>add user</Link>
+				<Link to="/about" className={`link ${selectedLink === 'about' ? 'active' : ''}`}  onClick={() => setSelectedLink('about')}>about</Link>
 			</nav>
 		</header>
 	)
